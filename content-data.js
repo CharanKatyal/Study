73ef5edb414cabc1417d1813bd499a47bdb1448a
chessmyfriend <chessmyfriend@gmail.com>
2025-12-31 09:55:17 +0000

    adding gitignore

--- content-data.js
+++ content-data.js
@@ -1,9 +1,5 @@
 export const fileSystemData = {
-  "Class 1": {
-    "fdfds": {
-      "content": "<p>fjkdhsfhkd fstest </p><p><br></p><p><br></p><p><span class=\"ql-formula\" data-value=\"e=mc^2\">﻿<span contenteditable=\"false\"><span class=\"katex\"><span class=\"katex-mathml\"><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><semantics><mrow><mi>e</mi><mo>=</mo><mi>m</mi><msup><mi>c</mi><mn>2</mn></msup></mrow><annotation encoding=\"application/x-tex\">e=mc^2</annotation></semantics></math></span><span class=\"katex-html\" aria-hidden=\"true\"><span class=\"base\"><span class=\"strut\" style=\"height: 0.43056em; vertical-align: 0em;\"></span><span class=\"mord mathnormal\">e</span><span class=\"mspace\" style=\"margin-right: 0.277778em;\"></span><span class=\"mrel\">=</span><span class=\"mspace\" style=\"margin-right: 0.277778em;\"></span></span><span class=\"base\"><span class=\"strut\" style=\"height: 0.814108em; vertical-align: 0em;\"></span><span class=\"mord mathnormal\">m</span><span class=\"mord\"><span class=\"mord mathnormal\">c</span><span class=\"msupsub\"><span class=\"vlist-t\"><span class=\"vlist-r\"><span class=\"vlist\" style=\"height: 0.814108em;\"><span class=\"\" style=\"top: -3.063em; margin-right: 0.05em;\"><span class=\"pstrut\" style=\"height: 2.7em;\"></span><span class=\"sizing reset-size6 size3 mtight\"><span class=\"mord mtight\">2</span></span></span></span></span></span></span></span></span></span></span></span>﻿</span> </p><p>dffd</p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p><p><br></p>"
-    }
-  },
+  "Class 1": {},
   "Class 2": {},
   "Class 3": {},
   "Class 4": {},
